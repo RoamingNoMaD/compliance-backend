43cc3fd0039d1e266fc584dc00210ae09decb329
Janos Laszlo Vasik <jvasik@redhat.com>
2025-04-09 14:52:40 +0200

    refactor(Kafka): use karafka for producing

--- karafka.rb
+++ karafka.rb
@@ -1,8 +1,29 @@
 # frozen_string_literal: true
 
 class KarafkaApp < Karafka::App
+  # librdkafka config creation
+  security_protocol = Settings.kafka.security_protocol.downcase
+
+  if security_protocol == 'sasl_ssl'
+    sasl_config = {
+      'sasl.username': Settings.kafka.sasl_username,
+      'sasl.password': Settings.kafka.sasl_password,
+      'sasl.mechanism': Settings.kafka.sasl_mechanism,
+      'security.protocol': Settings.kafka.security_protocol
+    }
+  else
+    sasl_config = {}
+  end
+
+  ca_location = Settings.kafka.ssl_ca_location if %w[ssl sasl_ssl].include?(security_protocol)
+
+  kafka_config = {
+    'bootstrap.servers': Settings.kafka.brokers,
+    'ssl.ca.location': ca_location
+  }.merge(sasl_config).compact
+
   setup do |config|
-    config.kafka = { 'bootstrap.servers': Settings.kafka.brokers }
+    config.kafka = kafka_config
     config.client_id = 'compliance_backend'
     config.consumer_persistence = !Rails.env.development?
   end
